23a71b862426ccc1bd265867254c83bbed78f170
Andreas <andmarknu@hotmail.com>
2024-01-24 20:47:12 +0100

    24.01.2024 20:47

---
+++ reacthooks/src/Page/Page.tsx
@@ -0,0 +1,12 @@
+import './Page.scss'
+import { Footer } from './Footer/Footer'
+export const Page = () => {
+
+    return(
+        <div className='Page'>
+            {/* <Header />
+            <Main /> */}
+            <Footer />         
+        </div>
+    )
+}
